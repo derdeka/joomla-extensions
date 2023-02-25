c4074130363fc7c916678e74f6523c6fc64f88bd
derdeka <derdeka@blackslash.de>
2023-02-25 19:12:26 +0100

    feat(com_adstxt): allow static entries

--- extensions/com_adstxt/views/plain/view.raw.php
+++ extensions/com_adstxt/views/plain/view.raw.php
@@ -3,10 +3,18 @@
 class AdstxtViewPlain extends JViewLegacy
 {
     public function display($tpl = null) {
+        $file = JPATH_SITE . '/images/ads.txt';
+
+        if (file_exists($file)) {
+            echo "# static\n";
+            echo preg_replace("~\r\n?~", "\n", file_get_contents($file));
+        }
+        
         $handle = fopen ("https://yieldlove.com/ads.txt", "r");
         
         if (!$handle) exit;
         
+        echo "# dynamic\n";
         while (($line = fgets($handle)) !== false) {
             if (preg_match("/^\s+$/", $line) or preg_match("/^#/", $line)) continue;
             echo $line;
